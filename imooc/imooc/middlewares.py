be61855b25591cb8d929d985612f0b711aceede0
huaweidong <huabitao@gmail.com>
2016-08-13 20:38:30 +0800

    因imooc页面改版，修改爬取元素

---
+++ imooc/imooc/middlewares.py
@@ -0,0 +1,37 @@
+__author__ = 'huafei'
+
+import random
+from scrapy import log
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+
+class RandomUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, settings, user_agent='Scrapy'):
+        super(RandomUserAgentMiddleware, self).__init__()
+        self.user_agent = user_agent
+
+    def process_request(self, request, spider):
+        ua = random.choice(self.user_agent_list)
+        if ua:
+            request.headers.setdefault('User-Agent', ua)
+            spider.log(
+                    u'User-Agent: {} {}'.format(request.headers.get('User-Agent'), request),
+                    level=log.DEBUG
+                )
+
+    """
+    the default user_agent_list composes chrome, IE, Firefox, Mozilla, Opera,
+    for more user agent strings, you can find it in http://www.useragentstring.com/pages/useragentstring.php
+    """
+    user_agent_list = [
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.45 Safari/535.19",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.45 Safari/535.19",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.11 Safari/535.19",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    ]
--- imooc/imooc/spiders/imooc_spider.py
+++ imooc/imooc/spiders/imooc_spider.py
@@ -3,34 +3,59 @@ __author__ = 'huafei'
 
 import scrapy
 import re
+import os
 from scrapy.selector import Selector
 from imooc.items import ImoocItem
 
 class ImoocSpider(scrapy.Spider):
     name = "imooc"
     allowed_domains = ["imooc.com"]
-    start_urls = ["http://www.imooc.com/course/list"]
+    start_urls = ["http://www.imooc.com/course/list?page=2"]
+
+    def __init__(self):
+        file = open("./spiders/keyword.txt", 'r')
+        self.keywords = []
+        lines = file.readlines()
+        for line in lines:
+            self.keywords.append(line.strip())
+
+    def extractKeywords(self, intro):
+        keyword = ""
+        first = True
+        for k in self.keywords:
+            if intro.lower().find(k.lower()) != -1:
+                if first:
+                    keyword = k
+                else :
+                    keyword = keyword + "|" + k
+        return keyword
 
     def parse(self, response):
         page = Selector(response)
-        divs = page.xpath('//li[@class="course-one"]')
+        divs = page.xpath('//div[@class="moco-course-wrap"]')
         base_urls = "http://www.imooc.com"
         for div in divs:
             item = ImoocItem()
-            item['name'] = div.xpath('./a/h5/span/text()').extract_first().encode('utf-8').strip()
-            item['url'] = div.xpath('./a/@href').extract_first()
-            item['img_url'] = div.xpath('./a/div[@class="course-list-img"]/img/@src').extract_first()
-            num = div.xpath('./a/div[@class="tips"]/span[@class="l ml20"]/text()').extract_first().encode('utf-8')
-            item['student_num'] = num.split()[0]
-            item['intro'] = div.xpath('./a/div[@class="tips"]/p[@class="text-ellipsis"]/text()').extract_first().encode('utf-8').strip()
-            url = base_urls + item['url']
-            print url
-            req = scrapy.Request(url, callback=self.parse_detail_page)
+            item['name'] = div.xpath('./a/div[@class="moco-course-box"]/div[@class="moco-course-intro"]/h3/text()').extract()[1].encode('utf-8').strip()
+            print item['name']
+            url = div.xpath('./a/@href').extract_first()
+            item['url'] = base_urls + url
+            print item['url']
+            item['id'] = url.encode('utf-8').strip().split('/')[2]
+            item['img_url'] = div.xpath('./a/div[@class="moco-course-box"]/img/@src').extract_first()
+            print item['img_url']
+            num = div.xpath('./a/div[@class="moco-course-box"]/div[@class="moco-course-bottom"]/span/text()').extract_first().encode('utf-8').strip()
+            print num
+            item['student_num'] = re.match(r'\d+', num).group(0).strip()
+            item['intro'] = div.xpath('./a/div[@class="moco-course-box"]/div[@class="moco-course-intro"]/p/text()').extract_first().encode('utf-8').strip()
+            print item['intro']
+            item['platform'] = 'imooc'
+            req = scrapy.Request(item['url'], callback=self.parse_detail_page)
             req.meta["item"] = item
             yield req
 
         curr_page = int(response.xpath('//div[@class="page"]/a[@class="active"]/text()').extract_first())
-        if curr_page != 29:
+        if curr_page != 2:
             next_page_url = base_urls + "/course/list?page=" + str(curr_page + 1)
             print "next page is ", next_page_url
             request = scrapy.Request(next_page_url, callback=self.parse)
@@ -41,7 +66,13 @@ class ImoocSpider(scrapy.Spider):
     def parse_detail_page(self, response):
         item = response.meta["item"]
 
-        item['grade'] = response.xpath('//div[@class="satisfaction-degree-info"]/h4/text()').extract_first().encode('utf-8').strip()
+        item['grade'] = response.xpath('//div[@class="score-info"]/div[@class="satisfaction-degree-info"]/h4/text()').extract_first().encode('utf-8').strip()
+        print item['grade']
         discuss_num = response.xpath('//p[@class="person-num noLogin"]/a/text()').extract_first().encode('utf-8')
         item['discuss_num'] = re.match(r'\d+', discuss_num).group(0).strip()
+        print item['discuss_num']
+        item['intro_detail'] = response.xpath('//div[@class="content"]/div[@class="course-brief"]/p/text()').extract_first().encode('utf-8').strip()
+        print item['intro_detail']
+        item['keywords'] = self.extractKeywords(item['intro_detail'])
+        print item['keywords']
         yield item
