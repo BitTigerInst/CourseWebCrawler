e37f40735a5300d90ca1ef02c9bcd64e6391f7d9
huaweidong <huabitao@gmail.com>
2016-08-28 00:42:21 +0800

    change sort

--- display/Display/rank/views.py
+++ display/Display/rank/views.py
@@ -10,10 +10,13 @@ db = client[dbName]
 courses = db['courses']
 
 def rank(request):
-	sortname = request.GET['sort']
-
-	ImoocList = courses.find({"platform":"imooc"}).sort([(sortname, -1)]).limit(10)
-	ClassCentralList = courses.find({"platform":{"$ne":"imooc"}}).sort([(sortname, -1)]).limit(10)
+	sortname = request.GET.get('sort', 'score')
+	if sortname == 'score':
+		secondsort = "review_num"
+	else:
+		secondsort = "score"
+	ImoocList = courses.find({"platform":"imooc"}).sort([(sortname, -1),(secondsort, -1)]).limit(10)
+	ClassCentralList = courses.find({"platform":{"$ne":"imooc"}}).sort([(sortname, -1),(secondsort, -1)]).limit(10)
 	return render(request, 'rank.html', {'ImoocList':ImoocList, 'ClassCentralList':ClassCentralList})
 
 
