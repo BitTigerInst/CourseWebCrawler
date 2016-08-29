ddf34ae56e608a3f5b6873d3dbcd25a29117cced
huaweidong <huabitao@gmail.com>
2016-08-30 02:01:32 +0800

    modify search

--- display/Display/search/views.py
+++ display/Display/search/views.py
@@ -1,27 +1,33 @@
+
 #coding:utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 import pymongo
+import json
 # Create your views here.
 
 dbName = 'imooc_class_central'
-client = pymongo.MongoClient('123.56.12.211', 27017)
+client = pymongo.MongoClient('localhost', 27017)
 db = client[dbName]
 courses = db['courses']
-ImoocCourseList = courses.find({"platform":"imooc"})
-ClassCentralCourseList = courses.find({"platform":{"$ne":"imooc"}})
-CourseNameList = ["aaa"]
-for item in ImoocCourseList:
-	CourseNameList.append(item["name"])
-for item in ClassCentralCourseList:
-	CourseNameList.append(item["name"])
+CourseList = courses.find()
+file = open("./search/keyword.txt", 'r')
+lines = file.readlines()
+CourseNameList = [line.lower().strip() for line in lines]        
+#for item in CourseList:
+#	CourseNameList.append(item["name"])
 
 def initial(request):
 	return render(request, 'result.html', {'CourseNameList':CourseNameList})
 
 def search(request):
-	searchname = request.GET.get('coursename')
-	ImoocSearchList = courses.find({"platform":"imooc", "name":searchname})
-	ClassCentralSearchList = courses.find({"platform":{"$ne":"imooc"}, "name":searchname})
-	return render(request, 'result.html', {'CourseNameList':CourseNameList, 'ImoocSearchList':ImoocSearchList, 'ClassCentralSearchList':ClassCentralSearchList})
+	searchname = request.GET.get('coursename', '')
+	if searchname == '':
+		ImoocSearchList = []
+		ClassCentralSearchList = []
+	else:
+		searchname = eval("\"" + searchname + "\"")
+		ImoocSearchList = courses.find({"platform":"imooc", "name":{"$regex":searchname, "$options":"$i"}})
+		ClassCentralSearchList = courses.find({"platform":{"$ne":"imooc"}, "name":{"$regex":searchname, "$options":"$i"}})
+	return render(request, 'result.html', {'CourseNameList':json.dumps(CourseNameList), 'ImoocSearchList':ImoocSearchList, 'ClassCentralSearchList':ClassCentralSearchList})
 
